1e3722d432861716df0d51cc367fd2526f23d59e
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2025-03-28 16:46:45 -0300

    Permitindo Cross-Origin Resource Sharing

--- api/src/server.js
+++ api/src/server.js
@@ -1,12 +1,24 @@
-import express from "express"; 
+import express from "express";
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { getData, getDateDiff, getMainPage } from "./client_routes.js";
+import cors from 'cors' 
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express()
+
+// Permite consulta entre domínios diferentes
+if (process.env.ENABLE_CORS === "true") {
+    const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
+    app.use(cors({ origin: allowedOrigins.length > 0 ? allowedOrigins : "*" }));
+    console.warn("\n\x1b[33mWarning\x1b[0m Cross-Origin Resource Sharing: true\n\x1b[33mWarning\x1b[0m Allowed origins:", allowedOrigins, "\n");
+} 
 
 // pega a rota absoluta 
 export const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const app = express()
 
 app.get('/', getMainPage)
 
